4a3d6634254100db34cbb7ee3954f80cddd01efe
restoran-kamila <122367135+restoran-kamila@users.noreply.github.com>
2023-01-17 17:40:22 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let banket_btn = document.getElementById('banket-btn'),
+    menu__box = document.getElementById('menu__box'),
+    contact_btn = document.getElementById('contact_btn')
+
+banket_btn.addEventListener('click',() => {
+    menu__box.style.visibility = 'hidden'
+})
+
+contact_btn.addEventListener('click', function() {
+    menu__box.style.visibility = 'hidden'
+})
+
+
